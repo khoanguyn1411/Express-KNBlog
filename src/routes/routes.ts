740ea22bc4f7341aefb5cc4beb0df1e40d24b02a
khoanguyn1411 <khoaah1411@gmail.com>
2023-04-03 12:55:25 +0700

    Change global routes to an appropriate name

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -7,9 +7,9 @@ import { blogRoutes } from "../features/blog/blog.routes";
 import { userRoutes } from "../features/user/user.routes";
 
 const router = Router();
-const globalRoutes = router.all("*", requireAuthorization);
+const allRoutesGuards = router.all("*", requireAuthorization);
 
 export function connectRoutes(app: Express) {
-  const routes = [globalRoutes, userRoutes, blogRoutes, authRoutes];
+  const routes = [allRoutesGuards, userRoutes, blogRoutes, authRoutes];
   app.use(...routes);
 }
