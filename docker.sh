8f50ddf8f3336f4f733bce375af86840e537b5c2
khoanguyn1411 <khoaah1411@gmail.com>
2023-11-09 23:56:07 +0700

    Config docker for server and db

---
+++ docker.sh
@@ -0,0 +1,16 @@
+rebuild() {
+   sudo docker builder prune -f
+   sudo docker compose build --no-cache
+   sudo docker compose up -d
+}
+
+stop() {
+   sudo docker compose down
+}
+
+start() {
+   sudo docker compose up -d
+}
+
+
+$@
