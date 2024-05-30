9005fc06f616c08ae707e4e2889bce5d87fe603d
khoanguyn1411 <khoaah1411@gmail.com>
2024-05-30 22:41:57 +0700

    Improve code style

--- index.ts
+++ index.ts
@@ -8,14 +8,24 @@ import { addCors } from "./src/configs/cors/add-cors";
 import { connectDatabase } from "./src/configs/db/connect-database";
 import { connectRoutes } from "./src/routes/routes";
 
+// Init all environment variables.
 connectEnv();
+
+// Init app.
 const app = express();
+
+// Add CORS config to allow access from FE app.
+addCors(app);
+
+// Body parser.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Connect and run database.
 connectDatabase();
+
+// Add routes.
 connectRoutes(app);
-addCors(app);
 
 app.listen(APP_PORT, () => {
   console.info(`KNBlog is listening on port ${APP_PORT}`);
