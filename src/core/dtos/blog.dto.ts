9cba098d9869e14f8dfbd46e41f5b98611bdcadd
khoanguyn1411 <khoaah1411@gmail.com>
2024-08-29 22:09:19 +0700

    Allow nullable for banner url

--- src/core/dtos/blog.dto.ts
+++ src/core/dtos/blog.dto.ts
@@ -21,7 +21,7 @@ export const blogCreationDtoSchema = Joi.object<BlogCreationDto>({
   title: Joi.string().required(),
   content: Joi.string().required(),
   summary: Joi.string().optional(),
-  bannerUrl: Joi.string().optional(),
+  bannerUrl: Joi.string().optional().allow(null),
 });
 
 export const blogQueryDtoSchema = paginationDtoSchema.append<BlogQueryDto>({
