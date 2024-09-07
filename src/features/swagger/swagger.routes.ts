31402e3fd19f68bc6c340c65311418df47f9b6d9
khoanguyn1411 <khoaah1411@gmail.com>
2024-09-07 23:25:36 +0700

    Add config

--- src/features/swagger/swagger.routes.ts
+++ src/features/swagger/swagger.routes.ts
@@ -1,29 +1,32 @@
 import { Router } from "express";
-import swaggerJSDoc from "swagger-jsdoc";
 import { serve, setup } from "swagger-ui-express";
 
 import { routePaths } from "@/routes/route-paths";
 
 import SwaggerJson from "./swagger.json";
 
-// Swagger definition
-const swaggerDefinition: swaggerJSDoc.Options["definition"] = {
-  openapi: "3.0.0",
-  info: {
-    title: "My Express API",
-    version: "1.0.0",
-    description: "API documentation for my Express application",
-  },
-};
-
-// Initialize swagger-jsdoc
-const swaggerSpec = swaggerJSDoc({
-  definition: swaggerDefinition,
-  apis: ["../**/*.routes.ts"],
-});
-
 const router = Router();
 
+/**
+ * Use this config if want to use swagger as JS docs.
+ * @see https://www.npmjs.com/package/swagger-jsdoc?activeTab=readme
+ * @example
+ */
+// // Swagger definition
+// const swaggerDefinition: swaggerJSDoc.Options["definition"] = {
+//   openapi: "3.0.0",
+//   info: {
+//     title: "My Express API",
+//     version: "1.0.0",
+//     description: "API documentation for my Express application",
+//   },
+// };
+
+// // Initialize swagger-jsdoc
+// const swaggerSpec = swaggerJSDoc({
+//   definition: swaggerDefinition,
+//   apis: ["../**/*.routes.ts"],
+// });
 router.use(routePaths.docs.url, serve, setup(SwaggerJson));
 
 export const swaggerRoutes = router.all("*");
