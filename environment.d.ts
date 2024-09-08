225b611a93b5323dc1dcea1bb25470049ae4d9d6
khoanguyn1411 <khoaah1411@gmail.com>
2024-09-08 13:31:28 +0700

    Remove redundant code

--- environment.d.ts
+++ environment.d.ts
@@ -15,18 +15,6 @@ declare global {
       readonly GOOGLE_CLIENT_SECRET: string;
       readonly GOOGLE_API_KEY: string;
 
-      readonly GOOGLE_SERVICE_ACCOUNT_TYPE: string;
-      readonly GOOGLE_SERVICE_ACCOUNT_PROJECT_ID: string;
-      readonly GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY_ID: string;
-      readonly GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY: string;
-      readonly GOOGLE_SERVICE_ACCOUNT_CLIENT_EMAIL: string;
-      readonly GOOGLE_SERVICE_ACCOUNT_CLIENT_ID: string;
-      readonly GOOGLE_SERVICE_ACCOUNT_AUTH_URI: string;
-      readonly GOOGLE_SERVICE_ACCOUNT_TOKEN_URI: string;
-      readonly GOOGLE_SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL: string;
-      readonly GOOGLE_SERVICE_ACCOUNT_CLIENT_X509_CERT_URL: string;
-      readonly GOOGLE_SERVICE_ACCOUNT_UNIVERSE_DOMAIN: string;
-
       readonly GOOGLE_DRIVE_STORAGE_LOCATION_ID: string;
     }
   }
